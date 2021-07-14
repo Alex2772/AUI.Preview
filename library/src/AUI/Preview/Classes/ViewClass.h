1060d49cf1ee7ecb4cbbd57073c918fc49829ee2
Alex2772 <alex2772sc@gmail.com>
2021-07-14 18:02:51 +0000

    added expanding

--- library/src/AUI/Preview/Classes/ViewClass.h
+++ library/src/AUI/Preview/Classes/ViewClass.h
@@ -7,6 +7,7 @@
 #include "ClassDescriptor.h"
 #include <AUI/Preview/Cpp/Runtime/Class.h>
 #include <AUI/View/AView.h>
+#include <AUI/View/AButton.h>
 
 template<>
 struct class_descriptor<AView>: Runtime::Class<AView>, Runtime::IClass {
@@ -15,6 +16,17 @@ public:
     ~class_descriptor() override = default;
 };
 
+template<typename T>
+struct view_class_descriptor: class_descriptor<AView>, Runtime::Class<T> {
+public:
+    view_class_descriptor() = default;
+    ~view_class_descriptor() override = default;
+};
 
 
+template <>
+struct class_descriptor<AButton>: view_class_descriptor<AButton> {};
+
+template <>
+struct class_descriptor<ALabel>: view_class_descriptor<ALabel> {};
 
